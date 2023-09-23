89e41ab788d74dff80fc08d067859b0d97640bd7
Álvaro San José <alvaro.sanjose@innocv.com>
2023-09-23 18:12:42 +0200

    Maquetas

--- src/views/Config.vue
+++ src/views/Config.vue
@@ -36,25 +36,57 @@ import { RouterLink, RouterView } from 'vue-router'
 
 <template>
   <main class="container">
-    <!-- <div>
-      <span>Define el contenido que quieres que sea moderado</span>
-      <v-textarea class="input" label="Promt" variant="outlined" v-model="prompt"></v-textarea>
-    </div> -->
-    <div class="categories">
-      <div class="category-item"><div class="title">Categorias</div><div class="title slider">Sensibilidad</div></div>
-      <div class="category-item" v-for="cat of categories">
-        <v-checkbox :label="cat.name" v-model="cat.active"></v-checkbox>
-        <v-slider class="slider" v-model="cat.sesibility" :disabled="!cat.active"></v-slider>
-        <v-select label="Select" :items="punishment" item-title="name" item-value="value" variant="outlined" v-model="cat.punishment" :disabled="!cat.active"></v-select>
+    
+      <div class="data">
+        <div class="title1">Twitch</div>
+        <div class="data-container">
+          <div class="card-data">
+            <div class="title">Baneados</div>
+            <div class="num">18</div>
+            <div class="subtitle">2 Completados</div>
+          </div>
+          <div class="card-data">
+            <div class="title">Contenido filtrado</div>
+            <div class="num">132</div>
+            <div class="subtitle">28 Completados</div>
+          </div>
+          <div class="card-data">
+            <div class="title">Comentarios borrados</div>
+            <div class="num">12</div>
+            <div class="subtitle">2 Completados</div>
+          </div>
+          <div class="card-data">
+            <div class="title">Salud</div>
+            <div class="num">76%</div>
+            <div class="subtitle">5% Completados</div>
+          </div>
+        </div>
       </div>
-      <v-btn @click="send">Enviar</v-btn>
-    </div>
+   
+    <v-card class="card">
+      <div class="categories">
+        <div class="category-item"><div class="title">Categorias</div><div class="title slider">Sensibilidad</div></div>
+        <div class="category-item" v-for="cat of categories">
+          <v-checkbox :label="cat.name" v-model="cat.active"></v-checkbox>
+          <v-slider class="slider" v-model="cat.sesibility" :disabled="!cat.active" step="0.05" min="0" max="1"></v-slider>
+          <v-select label="Select" :items="punishment" item-title="name" item-value="value" variant="outlined" v-model="cat.punishment" :disabled="!cat.active"></v-select>
+        </div>
+        <v-btn @click="send">Enviar</v-btn>
+      </div>
+    </v-card>
   </main>
 </template>
 
 <style>
   .container {
+    
+    background: #f2f2f2;
+  }
+  .card {
+    margin-left: 2rem;
+    margin-right: 2rem;
     padding: 4rem;
+    margin-bottom: 1rem;
   }
   .input {
     margin-top: 1rem;
@@ -66,8 +98,52 @@ import { RouterLink, RouterView } from 'vue-router'
   }
   .slider {
     grid-column: 2 / 4;
+    position: relative;
   }
+
+  /* .slider::before {
+    content: 'alto';
+    position: absolute;
+    top: -5px;
+    font-size: ;
+  } */
   .title {
     text-align: center;
+  }
+  
+  .data {
+
+    background-image: linear-gradient(to bottom, #624BFF 0%, #624BFF 75%, #f2f2f2 75%, #f2f2f2 100%);
+
+    margin-bottom: 2rem;
+  }
+  .data .title1 {
+    color: white;
+    font-weight: 800;
+    font-size: 3rem;
+    text-align: center;
+  }
+  .data-container {
+    padding-top: 2rem;
+    padding-right: 4rem;
+    padding-left: 4rem;
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    gap: 1rem;
+
+  }
+  .card-data {
+    border-radius: 10px;
+    padding: 1rem;
+    background: white;
+  }
+  .card-data .title {
+    font-weight: 800;
+  }
+  .card-data .num {
+    font-size: 2rem;
+  }
+  .card-data .subtitle {
+    font-size: 0.75rem;
   }
 </style>
